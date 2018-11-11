d15b4ee87972b3a881718fd84536dd6fa15e6b39
Consolate <cayoochan1@gmail.com>
2018-11-11 13:33:14 +0300

    Initial commit

---
+++ conditional_logic.py
@@ -0,0 +1,14 @@
+
+#this is input
+year=input ('what is your year of birth')
+
+#This is output
+print ('My year of birth is:', year)
+
+ 
+ #this is a variable
+ 
+current_year=2018
+age = year - current_year
+print('my age is :', age)
+
