958b67d2701af16c929410373b6bd9004aae193e
Ayoo Consolate Ochan <cayoochan1@gmail.com>
2018-11-22 16:13:09 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,4 @@
-def fizzbuzz(mylist,yourList):
-    mylist = [int(n) for n in input("Enter:").split(",")]
-    yourList = [int(n) for n in input("Enter:").split(",")] 
-
-    myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17, 18, 19, 20, 21]
-
-    yourList = [22, 23, 24, 25, 26, 27, 28, 29, 30, 31,32, 33, 34, 35, 36, 37, 38, 39, 40]
+def fizzbuzz(mylist = n for n in input("Enter:"), yourList = n for n in input("Enter:")):
          
     totallength = len(myList) + len(yourList)
 
@@ -22,4 +16,4 @@ def fizzbuzz(mylist,yourList):
     else:
         return totallength
     
-        print(fizzbuzz(myList,yourList))
+print(fizzbuzz())
