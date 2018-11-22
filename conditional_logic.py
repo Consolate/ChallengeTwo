98a5dc5e87ce4de313fe60f0392942717b39c41d
Ayoo Consolate Ochan <cayoochan1@gmail.com>
2018-11-22 14:51:42 +0300

    Update conditional_logic.py

--- conditional_logic.py
+++ conditional_logic.py
@@ -1,14 +1,14 @@
-
 #this is input
-year=input ('what is your year of birth')
-
-#This is output
-print ('My year of birth is:', year)
-
- 
+year=eval(input ('Enter your year of birth:'))
  #this is a variable
- 
-current_year=2018
-age = year - current_year
-print('my age is :', age)
+current_year=int(2018)
+age = current_year-year
+
+if age<18:
+    print('you are a minor')
+
+elif age == 18  and age <=36:
+    print('you are a youth')
+else:
+    print('You are an adult')
 
